9ac2ec9685e27922ace3bcd64315258ed6c08471
Dmytro Polishchuk <60980897+do-ping@users.noreply.github.com>
2023-12-03 10:19:12 -0500

    compare_tree function

--- helper_functions/tree.py
+++ helper_functions/tree.py
@@ -21,8 +21,8 @@ class TreeNode:
         if not values:
             return
 
-        root = cls(values[0])
-        queue = [root]
+        root: TreeNode = cls(values[0])
+        queue: List[TreeNode] = [root]
         i = 1
 
         while queue and i < len(values):
@@ -42,7 +42,37 @@ class TreeNode:
 
         return root
 
+    # def __eq__(self, other):
+    #     if not other:
+    #         return False
+    #     return (
+    #         self.val == other.val
+    #         and self.left == other.left
+    #         and self.right == other.right
+    #     )
+
     def __repr__(self) -> str:
         left_repr = f", left={repr(self.left)}" if self.left else ""
         right_repr = f", right={repr(self.right)}" if self.right else ""
         return f"TreeNode({self.val}{left_repr}{right_repr})"
+
+
+def compare_tree(this: Optional[TreeNode], that: Optional[TreeNode]) -> bool:
+    queue = [(this, that)]
+
+    while queue:
+        l, r = queue.pop()
+
+        if not l and not r:
+            continue
+
+        if l is None or r is None:
+            return False
+
+        if l.val != r.val:
+            return False
+
+        queue.append((l.left, r.left))
+        queue.append((l.right, r.right))
+
+    return not queue

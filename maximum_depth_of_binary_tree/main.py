85949dc74d777babc1641b83f541820fea2d08f8
Dmytro Polishchuk <60980897+do-ping@users.noreply.github.com>
2023-12-03 09:18:55 -0500

    Moved definition of the binary tree node to helper_functions.tree

--- maximum_depth_of_binary_tree/main.py
+++ maximum_depth_of_binary_tree/main.py
@@ -1,43 +1,6 @@
 from typing import Optional
 
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    @classmethod
-    def from_list(cls, values: list[int | None]):
-        if not values:
-            return
-
-        root = cls(values[0])
-        queue = [root]
-        i = 1
-
-        while queue and i < len(values):
-            node = queue.pop(0)
-
-            if values[i] is not None:
-                node.left = cls(values[i])
-                queue.append(node.left)
-
-            i += 1
-
-            if i < len(values) and values[i] is not None:
-                node.right = cls(values[i])
-                queue.append(node.right)
-
-            i += 1
-
-        return root
-
-    def __repr__(self):
-        left_repr = f", left={repr(self.left)}" if self.left else ""
-        right_repr = f", right={repr(self.right)}" if self.right else ""
-        return f"TreeNode({self.val}{left_repr}{right_repr})"
+from helper_functions.tree import TreeNode
 
 
 class Solution:
